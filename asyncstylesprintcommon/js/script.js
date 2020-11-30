4daea798b39a7a5629456cfe97f37d2545d84b1f
NastyaSkoblova1 <nastyaskoblova@mail.ru>
2020-11-30 18:34:15 +0300

    Async

--- asyncstylesprintcommon/js/script.js
+++ asyncstylesprintcommon/js/script.js
@@ -32,7 +32,7 @@ function t396_init(recid){var data='';var res=t396_detectResolution();t396_initT
 function t_onFuncLoad(name, ok_func, time) {
     // name = name.replace('\'', '');
     console.log(name);
-    name = returnName(name);
+    name = returnName(window[name]);
     console.log(name);
     if (name === 'function') {
         console.log(1);
