4b247f72e8dfb401537607ff57104b8eed8aff5a
NastyaSkoblova1 <nastyaskoblova@mail.ru>
2020-12-28 14:50:58 +0300

    Async

--- check/tilda-scripts-2.8.js
+++ check/tilda-scripts-2.8.js
@@ -1,30 +1,30 @@
-// window.isSearchBot = false;
-// if (/Bot/i.test(navigator.userAgent)) {
-//     window.isSearchBot = true;
-// }
+window.isSearchBot = false;
+if (/Bot/i.test(navigator.userAgent)) {
+    window.isSearchBot = true;
+}
 
-// window.isMobile = false;
-// window.$isMobile = false;
-// if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-//     window.isMobile = true;
-//     window.$isMobile = true;
-// }
+window.isMobile = false;
+window.$isMobile = false;
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    window.isMobile = true;
+    window.$isMobile = true;
+}
 
-// window.isiOS = false;
-// if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-//     window.isiOS = true;
-// }
+window.isiOS = false;
+if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    window.isiOS = true;
+}
 
-// window.isiOSVersion = '';
-// if(window.isiOS) {
-//     var version = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
-//     if (version !== null) {
-//         window.isiOSVersion = [parseInt(version[1], 10), parseInt(version[2], 10), parseInt(version[3] || 0, 10)];
-//     }
-// }
+window.isiOSVersion = '';
+if(window.isiOS) {
+    var version = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+    if (version !== null) {
+        window.isiOSVersion = [parseInt(version[1], 10), parseInt(version[2], 10), parseInt(version[3] || 0, 10)];
+    }
+}
 
-// window.browserLang = (window.navigator.userLanguage || window.navigator.language).toUpperCase().slice(0, 2);
-// window.tildaBrowserLang = window.browserLang;
+window.browserLang = (window.navigator.userLanguage || window.navigator.language).toUpperCase().slice(0, 2);
+window.tildaBrowserLang = window.browserLang;
 
 // (function ($) {
 //     $(document).ready(function () {
@@ -1958,40 +1958,40 @@
 //     };
 // })(jQuery);
 
-// function t_throttle(fn, threshhold, scope) {
-//     threshhold || (threshhold = 250);
-//     var last, deferTimer;
-//     return function () {
-//         var context = scope || this;
+function t_throttle(fn, threshhold, scope) {
+    threshhold || (threshhold = 250);
+    var last, deferTimer;
+    return function () {
+        var context = scope || this;
 
-//         var now = +new Date(),
-//             args = arguments;
-//         if (last && now < last + threshhold) {
-//             clearTimeout(deferTimer);
-//             deferTimer = setTimeout(function () {
-//                 last = now;
-//                 fn.apply(context, args);
-//             }, threshhold);
-//         } else {
-//             last = now;
-//             fn.apply(context, args);
-//         }
-//     };
-// }
+        var now = +new Date(),
+            args = arguments;
+        if (last && now < last + threshhold) {
+            clearTimeout(deferTimer);
+            deferTimer = setTimeout(function () {
+                last = now;
+                fn.apply(context, args);
+            }, threshhold);
+        } else {
+            last = now;
+            fn.apply(context, args);
+        }
+    };
+}
 
-// function t_onFuncLoad(funcName, okFunc, time) {
-//     if (typeof window[funcName] === 'function') {
-//         okFunc();
-//     } else {
-//         var timerId = setTimeout(function checkFuncExist() {
-//             if (typeof window[funcName] === 'function') {
-//                 okFunc();
-//                 return;
-//             }
-//             timerId = setTimeout(checkFuncExist, time || 100);
-//         });
-//     }
-// }
+function t_onFuncLoad(funcName, okFunc, time) {
+    if (typeof window[funcName] === 'function') {
+        okFunc();
+    } else {
+        var timerId = setTimeout(function checkFuncExist() {
+            if (typeof window[funcName] === 'function') {
+                okFunc();
+                return;
+            }
+            timerId = setTimeout(checkFuncExist, time || 100);
+        });
+    }
+}
 
 // window.Tilda = window.Tilda || {};
 // (function ($) {
