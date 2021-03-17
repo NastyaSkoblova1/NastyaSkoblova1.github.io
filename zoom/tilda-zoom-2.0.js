7789fe3d10055d37043322fa9de3c36b3e0088e7
NastyaSkoblova1 <nastyaskoblova@mail.ru>
2021-03-17 18:14:38 +0300

    zoom

--- zoom/tilda-zoom-2.0.js
+++ zoom/tilda-zoom-2.0.js
@@ -201,6 +201,7 @@ function t_zoom_initSwipe() {
     var pos = parseInt($('.t-carousel__zoomer__item:visible').attr('data-zoomer-slide-number'), 10);
 
     if (slideItem.length > 1) {
+        
         // hammer = new Hammer($('.t-zoomer__wrapper')[0], {
             // domEvents: true,
             // inputClass: Hammer.TouchInput,
