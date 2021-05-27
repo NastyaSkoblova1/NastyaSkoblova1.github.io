c19c0845ef50418e86e9a19ac2c5520ebf1ee492
NastyaSkoblova1 <nastyaskoblova@mail.ru>
2021-05-27 15:53:10 +0300

    Cover

--- cover/tilda-cover-1.0.js
+++ cover/tilda-cover-1.0.js
@@ -371,8 +371,8 @@ function cover_fixcontentheight(id) {
     var el = $('#rec' + id);
     var hcover = el.find('.t-cover').height();
     var hcontent = el.find('div[data-hook-content]').outerHeight();
-    console.log(hcover, hcontent);
-    console.log(hcontent > 300 && hcover < hcontent + 40);
+    console.log('Hcover ', hcover);
+    console.log('Hcontent ', hcontent);
     if (hcontent > 300 && hcover < hcontent + 40) {
         var hcontent = hcontent + 120;
         if (hcontent > 1000) {
@@ -408,26 +408,27 @@ function cover_fixcontentheight(id) {
                 console.log('error:' + e);
             }
         }
-    } else {
-        var hcontent = hcontent + 120;
-        if (hcontent > 1000) {
-            hcontent += 100;
-        }
-        // eslint-disable-next-line no-console
-        console.log('auto correct cover height: ' + hcontent);
-        el.find('.t-cover').height(hcontent);
-        el.find('.t-cover__filter').height(hcontent);
-        el.find('.t-cover__carrier').height(hcontent);
-        el.find('.t-cover__wrapper').height(hcontent);
-        if (typeof window.t_lazyload_updateResize_elem === 'function') {
-            try {
-                window.t_lazyload_updateResize_elem(el.find('.t-cover__carrier'));
-            } catch (e) {
-                // eslint-disable-next-line no-console
-                console.log('error:' + e);
-            }
-        }
-    }
+    } 
+    // else {
+    //     var hcontent = hcontent + 120;
+    //     if (hcontent > 1000) {
+    //         hcontent += 100;
+    //     }
+    //     // eslint-disable-next-line no-console
+    //     console.log('auto correct cover height: ' + hcontent);
+    //     el.find('.t-cover').height(hcontent);
+    //     el.find('.t-cover__filter').height(hcontent);
+    //     el.find('.t-cover__carrier').height(hcontent);
+    //     el.find('.t-cover__wrapper').height(hcontent);
+    //     if (typeof window.t_lazyload_updateResize_elem === 'function') {
+    //         try {
+    //             window.t_lazyload_updateResize_elem(el.find('.t-cover__carrier'));
+    //         } catch (e) {
+    //             // eslint-disable-next-line no-console
+    //             console.log('error:' + e);
+    //         }
+    //     }
+    // }
 }
 
 
